8de828441cb6578e2860559123430a3a5c5bb935
inacioferrarini <inacio.ferrarini@gmail.com>
2012-10-29 23:08:06 -0200

    including Maven projects

---
+++ Maven/mvn-sandalone-archetype/src/main/resources/archetype-resources/src/test/java/AppTest.java
@@ -0,0 +1,22 @@
+package ${package};
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+	static final Logger logger = LoggerFactory.getLogger(AppTest.class);
+	
+	@Test
+	public void test() {
+		logger.trace("Trace Level Message");
+		logger.debug("Debug Level Message");
+		logger.info("Info Level Message");
+		logger.warn("Warn Level Message");
+		logger.error("Error Level Message");		
+	}	
+}
